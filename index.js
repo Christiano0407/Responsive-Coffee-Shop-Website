5d2e15c4670633bc92dae49489ccb250f7127d4e
Christiano0407 <placebo0704@gmail.com>
2022-05-04 08:49:28 -0500

    menu icon

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //** === Coffee Design ==  */
+const menu = document.querySelector("#menuBtn");
+const btnBar = document.querySelector("#btnBar");
+const navBar = document.querySelector(".header .flex .navBar");
+
+btnBar.addEventListener("click", () => {
+  btnBar.classList.toggle("fa-times");
+  //<i class="fa-solid fa-xmark"></i>;
+  //<i class="fa-solid fa-square-xmark"></i>;
+});
