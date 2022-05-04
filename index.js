ba66048ab6f3cb0a28bc35773e73469c03796150
Christiano0407 <placebo0704@gmail.com>
2022-05-04 09:19:45 -0500

    menu

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 //** === Coffee Design ==  */
 const menu = document.querySelector("#menuBtn");
 const btnBar = document.querySelector("#btnBar");
-const navBar = document.querySelector(".header .flex .navBar");
+const navbar = document.querySelector(".header .flex .navBar");
 
 btnBar.addEventListener("click", () => {
   btnBar.classList.toggle("fa-times");
-  //<i class="fa-solid fa-xmark"></i>;
-  //<i class="fa-solid fa-square-xmark"></i>;
+  navbar.classList.toggle("active");
 });
